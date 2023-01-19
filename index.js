4523623897a1ebce92285cb97d180667b9188d98
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-01-19 20:02:23 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 let charactersOptions = characters;
 let passwordOne = "";
 let passwordTwo = "";
-let passwordOneElement = document.querySelector("#password1-el");
-let passwordTwoElement = document.querySelector("#password2-el");
-let passwordLengthElement = document.querySelector("#password-length");
+const passwordOneElement = document.querySelector("#password1-el");
+const passwordTwoElement = document.querySelector("#password2-el");
+const passwordLengthElement = document.querySelector("#password-length");
 let passwordLength = passwordLengthElement.value;
 let symbolsChecked = false;
 let numbersChecked = false;
-let symbolsCheckbox = document.querySelector("#symbols");
-let numbersCheckbox = document.querySelector("#numbers");
+const symbolsCheckbox = document.querySelector("#symbols");
+const numbersCheckbox = document.querySelector("#numbers");
 
 function generate() {
 passwordLength = getPasswordLength();
